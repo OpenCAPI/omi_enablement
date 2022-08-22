d4867a6105526947d2bcc89345d9533004f61f0b
Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>
2022-08-22 17:48:31 +0200

    Update script.sh

--- petalinux/script.sh
+++ petalinux/script.sh
@@ -1,6 +1,7 @@
 petalinux-create --type project -s *.bsp
 mkdir vcuomi/images && mkdir vcuomi/images/linux
 cat linux/images/vmlinuxa* > linux/images/vmlinux
+rm linux/images/vmlinuxa*
 cp linux/images/rootfs.cpio.gz linux/images/rootfs.cpio2.gz
 gunzip linux/images/rootfs.cpio2.gz
 mv linux/images/rootfs.cpio2 linux/images/rootfs.cpio
