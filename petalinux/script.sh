9ac5d64a17c9f39aff5cfdaa45757b04fab1f819
Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>
2022-08-22 14:57:09 +0200

    dealing with large files
    Signed-off-by: Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>

--- petalinux/script.sh
+++ petalinux/script.sh
@@ -1,3 +1,8 @@
 petalinux-create --type project -s *.bsp
 mkdir vcuomi/images && mkdir vcuomi/images/linux
+cat linux/images/vmlinuxa* > linux/images/vmlinux
+cp linux/images/rootfs.cpio.gz linux/images/rootfs.cpio2.gz
+gunzip linux/images/rootfs.cpio2.gz
+mv linux/images/rootfs.cpio2 linux/images/rootfs.cpio
 cp -r linux/images/* vcuomi/images/linux/.
+
